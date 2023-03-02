0d410fe76dcfd82224905dc86ac4172cb8414231
maikaodev <maikao.dev@gmail.com>
2023-03-02 19:25:26 -0300

    feat: configuring the quasar

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,39 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="q-pa-md">
+    <q-layout
+      view="lHh lpr lFf"
+      container
+      style="height: 400px"
+      class="shadow-2 rounded-borders"
+    >
+      <q-header elevated class="bg-purple">
+        <q-toolbar>
+          <q-btn flat round dense icon="menu" class="q-mr-sm" />
+          <q-space></q-space>
+          <q-btn flat round dense icon="search" class="q-mr-xs" />
+          <q-btn flat round dense icon="group_add" />
+        </q-toolbar>
+        <q-toolbar inset>
+          <q-toolbar-title> <strong>Quasar</strong> Framework </q-toolbar-title>
+        </q-toolbar>
+      </q-header>
+
+      <q-page-container>
+        <q-page class="q-pa-md">
+          <p v-for="n in 15" :key="n">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nihil
+            praesentium molestias a adipisci, dolore vitae odit, quidem
+            consequatur optio voluptates asperiores pariatur eos numquam rerum
+            delectus commodi perferendis voluptate?
+          </p>
+        </q-page>
+      </q-page-container>
+    </q-layout>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
