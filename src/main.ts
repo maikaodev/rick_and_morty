82b5ee282413762772e0fc4654f31cfe7fa6cb18
maikaodev <maikao.dev@gmail.com>
2023-03-02 19:25:26 -0300

    feat: configuring the quasar

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,22 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+// FILE: main.js
 
-createApp(App).mount('#app')
+import { createApp } from "vue";
+import { Quasar } from "quasar";
+import quasarIconSet from "quasar/icon-set/fontawesome-v6";
+
+import "@quasar/extras/roboto-font/roboto-font.css";
+import "@quasar/extras/material-icons/material-icons.css";
+import "@quasar/extras/fontawesome-v6/fontawesome-v6.css";
+
+import "quasar/src/css/index.sass";
+
+import App from "./App.vue";
+
+const myApp = createApp(App);
+
+myApp.use(Quasar, {
+  plugins: {},
+  iconSet: quasarIconSet,
+});
+
+myApp.mount("#app");
