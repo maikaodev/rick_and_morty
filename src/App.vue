d7880ac07e5a2a401e6d2a586a3f531ae0ac3afd
maikaodev <maikao.dev@gmail.com>
2023-03-02 19:46:17 -0300

    feat: configuring routes

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,13 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
+import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-  <div class="q-pa-md">
-    <q-layout
-      view="lHh lpr lFf"
-      container
-      style="height: 400px"
-      class="shadow-2 rounded-borders"
-    >
-      <q-header elevated class="bg-purple">
-        <q-toolbar>
-          <q-btn flat round dense icon="menu" class="q-mr-sm" />
-          <q-space></q-space>
-          <q-btn flat round dense icon="search" class="q-mr-xs" />
-          <q-btn flat round dense icon="group_add" />
-        </q-toolbar>
-        <q-toolbar inset>
-          <q-toolbar-title> <strong>Quasar</strong> Framework </q-toolbar-title>
-        </q-toolbar>
-      </q-header>
-
-      <q-page-container>
-        <q-page class="q-pa-md">
-          <p v-for="n in 15" :key="n">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nihil
-            praesentium molestias a adipisci, dolore vitae odit, quidem
-            consequatur optio voluptates asperiores pariatur eos numquam rerum
-            delectus commodi perferendis voluptate?
-          </p>
-        </q-page>
-      </q-page-container>
-    </q-layout>
+  <div>
+    <router-link :to="{ name: 'home' }">Go to Home</router-link>
+    <router-link :to="{ name: 'about' }">About</router-link>
   </div>
+  <router-view />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
